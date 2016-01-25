ca9ba9211486da1b6756dc34779257044702280f
brooke hurford and lindsey raffa <lindsey.raffa@gmail.com>
2016-01-25 09:07:53 -0800

    add address field

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
-function Contact(firstName, lastName) {
+function Contact(firstName, lastName, city, state) {
   this.firstName = firstName;
   this.lastName = lastName;
+  this.city = city;
+  this.state = state;
   this.addresses = [];
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
+}
+
+Contact.prototype.fullAddress = function() {
+  return this.city + ", " + this.state;
 }
 
 $(document).ready(function() {
@@ -14,18 +20,25 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var inputtedCity = $("input#new-city").val();
+    var inputtedState = $("input#new-state").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedCity, inputtedState);
 
-     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + " " + newContact.fullAddress() + "</span></li>");
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
-  });
+    $("input#new-city").val("");
+    $("input#new-state").val("");
 
-  $(".contact").last().click(function() {
-  $("#show-contact").show();
-  $("#show-contact h2").text(newContact.fullName());
-  $(".first-name").text(newContact.firstName);
-  $(".last-name").text(newContact.lastName);
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.fullName());
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $(".city").text(newContact.city);
+      $(".state").text(newContact.state);
+    });
+
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,12 +1,20 @@
-describe ("contact", function () {
+describe ("Contact", function () {
   it("create a new contact with the given properties", function() {
-    var testContact = new Contact("Rita", "Moreno");
+    var testContact = new Contact("Rita", "Moreno", "Eugene", "Oregon");
     expect(testContact.firstName).to.equal("Rita");
     expect(testContact.lastName).to.equal("Moreno");
+    expect(testContact.city).to.equal("Eugene");
+    expect(testContact.state).to.equal("Oregon");
     expect(testContact.addresses).to.eql([]);
   });
+
   it("adds the fullName method to a contact", function() {
     var testContact = new Contact("Sherlock", "Holmes");
     expect(testContact.fullName()).to.equal("Sherlock Holmes");
   });
-});
+
+  it("adds the fullAddress method to the contact", function() {
+    var testContact = new Contact("Jason", "Awbrey", "Portland", "Oregon");
+    expect(testContact.fullAddress()).to.equal("Portland, Oregon");
+    });
+  });
