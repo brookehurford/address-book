39543a93548bd9b85ccafc2bc62e4feb45b98880
brooke hurford and lindsey raffa <student@epicodus.com>
2016-01-25 16:21:36 -0800

    add changes to address book that includes address with bugs

--- spec/specs.js
+++ spec/specs.js
@@ -12,9 +12,19 @@ describe ("Contact", function () {
     var testContact = new Contact("Sherlock", "Holmes");
     expect(testContact.fullName()).to.equal("Sherlock Holmes");
   });
+});
 
-  it("adds the fullAddress method to the contact", function() {
-    var testContact = new Contact("Jason", "Awbrey", "Portland", "Oregon");
-    expect(testContact.fullAddress()).to.equal("Portland, Oregon");
-    });
+describe ("Address", function() {
+  it("creates a new address with the given specifications", function() {
+    var testAddress = new Address("111 Miller St", "Golden", "Colorado");
+    expect (testAddress.street).to.equal("111 Miller St");
+    expect (testAddress.city).to.equal("Golden");
+    expect (testAddress.state).to.equal("Colorado");
   });
+
+  it("adds the fullAddress method to all addresses", function() {
+    var testAddress = new Address("904 SE 20th Ave", "Portland", "Oregon");
+    expect(testAddress.fullAddress()).to.equal("904 SE 20th Ave, Portland, Oregon");
+    });
+
+});
